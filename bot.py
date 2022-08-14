4bf4bcbb77ade3f60b2a931ddddb51acf1f16cd7
Alice S <snapp@null.net>
2022-08-14 16:50:05 -0400

    Initialize database reading and yaml parsing

--- bot.py
+++ bot.py
@@ -1,5 +1,13 @@
+
+import sqlite3
 import discord
+import yaml  # external lib: pip install pyyaml
 from discord.ext import commands
+from sqlite3 import Error
+try:
+	from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+	from yaml import Loader, Dumper
 
 intents = discord.Intents.default()
 """
@@ -13,10 +21,67 @@ intents.guilds   = True # Allows the bot to view the servers it belongs to.  "I'
 
 bot = commands.Bot(command_prefix='mi-', intents=intents)
 
+
+
+class Tempest:
+	def __init__(self):
+		with open('tempest.yaml', 'r') as file:
+			roles = yaml.load(file)
+
+class Database:
+
+	connection = None
+
+	def __init__(self):
+		cls = self.__class__
+		try:  # attempt to connect to the sqlite database "database"
+			cls.connection = sqlite3.connect("database.sqlite3")
+			print('Connected to database.')
+		except Error as e:
+			print(f'Unable to connect to database. {e}')
+			return # can't connect to database, skip this method.
+
+		create_members = '''
+		CREATE TABLE IF NOT EXISTS members (
+			id INTEGER PRIMARY KEY,
+			ign TEXT,
+			officer INTEGER, 
+			member BOOL NOT NULL
+			);
+		'''
+
+		create_inactive = '''
+		CREATE TABLE IF NOT EXISTS inactive (
+		id INTEGER PRIMARY KEY,
+		ti1 INTEGER NOT NULL,
+		ti2 INTEGER NOT NULL
+		);
+		'''
+
+		cursor = cls.connection.cursor()
+		cursor.execute(create_members)
+		cursor.execute(create_inactive)
+
+		cls.connection.commit()
+		# EOI
+
+	@classmethod
+	def add_member(cls, member, ign='NULL', officer='NULL'):
+		query = f"""
+		INSERT INTO
+		   members (id, member)
+		VALUES
+		   ({member.id}, '{ign}');
+		"""
+
+
+db = None
 @bot.event # lines of code that start with @ in Python are "Decorators".  They're basically methods that act on the method being declared on the line after.
 async def on_ready():
 	# this is the on_ready event.  This method will call once the bot is fully logged in.
 	print('Logged on as {0}!'.format(bot.user))
+	global db
+	db = Database()
 
 
 @bot.command()
@@ -39,4 +104,4 @@ async def yo(context):
 if __name__ == "__main__":  # make sure nothing else runs this part except for this code file
 
 	with open('key', 'r') as key:
-		bot.run(key.read())  # activates the bot using the token (DON'T SHARE THE TOKEN).
+		bot.run(key.readline())  # activates the bot using the token (DON'T SHARE THE TOKEN).
