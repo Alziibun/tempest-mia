7e46d497262863f22b9fecae19245573cdfad9d0
Alice S <snapp@null.net>
2022-08-19 12:50:43 -0400

    Move tempest class to separate module

--- bot.py
+++ bot.py
@@ -1,7 +1,5 @@
-
-import sqlite3
+import tempest
 import discord
-import yaml  # external lib: pip install pyyaml
 import datetime as dt
 from discord.ext import commands
 from sqlite3 import Error
@@ -24,66 +22,11 @@ extensions = ['inactive']
 
 bot = commands.Bot(command_prefix='mi-', intents=intents)
 
-class Tempest:
-	def __init__(self):
-		with open('tempest.yaml', 'r') as file:
-			roles = yaml.load(file)
-			print(roles)
-
-class Database:
-
-	connection = None
-
-	def __init__(self):
-		cls = self.__class__
-		try:  # attempt to connect to the sqlite database "database"
-			cls.connection = sqlite3.connect("database.sqlite3")
-			print('Connected to database.')
-		except Error as e:
-			print(f'Unable to connect to database. {e}')
-			return # can't connect to database, skip this method.
-
-		create_members = '''
-		CREATE TABLE IF NOT EXISTS members (
-			id INTEGER PRIMARY KEY,
-			ign TEXT,
-			officer INTEGER, 
-			member BOOL NOT NULL
-			);
-		'''
-
-		create_inactive = '''
-		CREATE TABLE IF NOT EXISTS inactive (
-		id INTEGER PRIMARY KEY,
-		ti1 INTEGER NOT NULL,
-		ti2 INTEGER NOT NULL
-		);
-		'''
-
-		cursor = cls.connection.cursor()
-		cursor.execute(create_members)
-		cursor.execute(create_inactive)
-
-		cls.connection.commit()
-		# EOI
-
-	@classmethod
-	def add_member(cls, member, ign='NULL', officer='NULL'):
-		query = f"""
-		INSERT INTO
-		   members (id, member)
-		VALUES
-		   ({member.id}, '{ign}');
-		"""
-
-
-db = None
 @bot.event # lines of code that start with @ in Python are "Decorators".  They're basically methods that act on the method being declared on the line after.
 async def on_ready():
 	# this is the on_ready event.  This method will call once the bot is fully logged in.
 	print('Logged on as {0}!'.format(bot.user))
-	global db
-	db = Database()
+	tempest.init(bot.get_guild(942230609701511228))
 
 @bot.event
 async def on_member_join(member):
