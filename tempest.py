70f6f3bc62e1fbc002e4450cec78960f07d40298
Alice S <snapp@null.net>
2022-08-19 12:48:48 -0400

    Update tempest.py

--- tempest.py
+++ tempest.py
@@ -6,48 +6,44 @@ try:
 except ImportError:
 	from yaml import Loader, Dumper
 
-class Tempest:
-	roles = dict()
-	def __init__(self, guild):
-		print(f'Initiating {guild.name} as Tempest server.')	
-		cls = self.__class__
-		with open('tempest.yaml', 'r') as file:
-			_roles = yaml.load(file, Loader=Loader)
-			for k, v in _roles.items():
-				cls.roles[k] = {
-					'av'  : v['access'],
-					'obj' : guild.get_role(v['id'])
-					}
-				print(cls.roles[k]['obj'].name, 'loaded.')
-	# END OF INITILIZATION
+server = None
+roles = dict()
+def init(guild: discord.Guild):
+	print(f'Initiating {guild.name} as Tempest server.')
+	global server, roles
+	server = guild
+	with open('tempest.yaml', 'r') as file:
+		_roles = yaml.load(file, Loader=Loader)
+		for k, v in _roles.items():
+			roles[k] = {
+				'av'  : v['access'],
+				'obj' : guild.get_role(v['id'])
+				}
 
-	@classmethod
-	def get_roles(cls):
-		# return roles as a list
-		return [i['obj'] for i in cls.roles.values()]
+def get_roles():
+	# return roles as a list
+	if not len(roles): return print('Sever uninitialized.  Unable to get roles.')
+	return [i['obj'] for i in roles.values()]
 
-	@classmethod
-	def get_rank(cls, member: discord.Member):
-		# find the guild rank of a discord member
-		for role in cls.roles.values():
-			if role['obj'] in member.roles:
-				return role
+def get_rank(member: discord.Member):
+	# find the guild rank of a discord member
+	if not len(roles): return print('Server uninitialized. Unable to get roles.')
+	for role in roles.values():
+		if role['obj'] in member.roles:
+			return role
 
-	@classmethod
-	def has_access(cls, member: discord.Member, req):
-		# check if the member has the required av for the command
-		if cls.get_rank(member)['av'] <= req:
-			return True
-		return False
+def has_access(member: discord.Member, req):
+	# check if the member has the required av for the command
+	return True if get_rank(member)['av'] <= req else False
 
 class Database:
-
-	connection = None
-
+	con = None
+	cur = None
 	def __init__(self):
 		cls = self.__class__
 		try:  # attempt to connect to the sqlite database "database"
-			cls.connection = sqlite3.connect("database.sqlite3")
+			cls.con = sqlite3.connect("database.sqlite3")
+			cls.cur = cls.con.cursor()
 			print('Connected to database.')
 		except Error as e:
 			print(f'Unable to connect to database. {e}')
@@ -70,23 +66,28 @@ class Database:
 		);
 		'''
 
-		cursor = cls.connection.cursor()
-		cursor.execute(create_members)
-		cursor.execute(create_inactive)
+		cls.cur.execute(create_members)
+		cls.cur.execute(create_inactive)
 
-		cls.connection.commit()
+		cls.con.commit()
 		# END OF INITIALIZATION
 
 	@classmethod
-	def add_member(cls, member: discord.Member, ign='NULL', officer='NULL'):
+	def commit(cls, *query):
+		for q in query:
+			cls.cur.execute(q)
+		cls.con.commit()
+
+	@classmethod
+	def add_member(cls, member: discord.Member, ign='NULL', officer='NULL', ismember=False):
 		query = f"""
 		INSERT INTO
-		   members (id, ign)
+		   members (id, ign, officer, member)
 		VALUES
-		   ({member.id}, '{ign}');
+		   ({member.id}, '{ign}', '{officer}', {ismember});
 		"""
-		cursor.execute(query)
-		cls.connection.commit()
+		
+		cls.commit(query)
 		print(f'Added {member.name} to database.')
 
 	@classmethod
@@ -98,6 +99,20 @@ class Database:
 			({member.id}, {ti1}, {ti2})
 		"""
 
-		cursor = cls.connection.cursor()
-		cursor.execute(query)
-		cls.connection.commit()
+		cls.commit(query)
+		print(f'Inactivity registered to database.')
+
+	@classmethod
+	def get_member(cls, member: discord.Member):
+		try:
+			return cls.cur.execute(f'SELECT * FROM members WHERE id={member.id}').fetchone()
+		except:
+			return print('Unable to find', member.name, 'in database.')
+
+	@classmethod
+	def get_member_by_ign(cls, tof: str):
+		try:
+			return cls.cur.execute(f"SELECT * FROM members WHERE tof='{tof}'")
+		except:
+			return print('Unable to find Tower of Fantasy username:', tof, 'in database.')
+
