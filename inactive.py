3e7166eb443fa7c693cdf7ab1a4f364d40048688
Alice S <snapp@null.net>
2022-08-19 12:49:26 -0400

    Low-operational inactivity profiles

--- inactive.py
+++ inactive.py
@@ -1,39 +1,38 @@
 import discord
 import math
 import datetime as time
-from tempest import Tempest, Database
+import tempest
 from datetime import timedelta
 from discord.ext import commands
 
 class Inactivity(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
-		self.tempest = Tempest( await bot.fetch_guild(942230609701511228) )
 
 	@commands.command(aliases=['ireg'])
 	@commands.guild_only()
-	async def inactive(self, ctx, name: str, days: int):
-		if not self.tempest.has_access(ctx.author): return print('lol')
+	async def inactive(self, ctx, name: str, days: int, *, note='None provided.'):
+		if not tempest.has_access(ctx.author, 3): return print('lol')
+		print(note)
 		"""
 		- Register the {name} to the inactivity registry for {time} days.
 		- Temporarily using primitive and usually unreliable discord.py functions for now
 		- Temporarily using days as a default delta unless Reya needs it to be longer than that.
 
 		"""
-
 		# (CASE SENSITIVE) Looks for a member with the same name or nickname.  If a discriminator is provided, looks for that user directly.
 		member = ctx.guild.get_member_named(name)
 		period = time.datetime.now() + timedelta(days=days)
 		embed = discord.Embed(
 			title 		= member.name,
-			description = f"Discord: {member.mention}\nToF: ???") # TOF name assignments TBA
+			description = f"Discord: {member.mention}\nToF: *Coming soon*") # TOF name assignments TBA
 		embed.add_field(
 			name  = 'Expected Return Time',
 			value = f"<t:{math.floor(period.timestamp())}:R>",
 			inline= False)
 		embed.add_field(
 			name  = "Note",
-			value = "*None*") # note adding TBA
+			value = f"*{note}*") # note adding TBA
 		embed.set_thumbnail(url=member.avatar_url)
 		embed.set_footer(
 			text     = ctx.author.display_name,
