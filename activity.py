2e208aa4b4afd37aef57193450d884bd9ec10b25
Alice S <snapp@null.net>
2022-08-26 15:00:38 -0400

    add access checks

--- activity.py
+++ activity.py
@@ -11,14 +11,15 @@ class Activity(commands.Cog):
 		self.bot = bot
 
 	@commands.command(aliases=['ireg'])
+	@tempest.access(3)
 	async def inactive(self, ctx, name: str, days: int, *, note='None provided.'):
-		if not tempest.has_access(ctx.author, 3): return print('lol')
-		print(note)
+		"""
+		Write an inactivity note.
+		"""
 		"""
 		- Register the {name} to the inactivity registry for {time} days.
 		- Temporarily using primitive and usually unreliable discord.py functions for now
 		- Temporarily using days as a default delta unless Reya needs it to be longer than that.
-
 		"""
 		# (CASE SENSITIVE) Looks for a member with the same name or nickname.  If a discriminator is provided, looks for that user directly.
 		member = ctx.guild.get_member_named(name)
@@ -39,12 +40,15 @@ class Activity(commands.Cog):
 		embed.set_footer(
 			text     = ctx.author.display_name,
 			icon_url = ctx.author.avatar.url)
-
 		channel = ctx.guild.get_channel(1008223604170825788)
 		await channel.send(embed=embed)
 
 	@commands.command(aliases=['o'])
+	@tempest.access(3)
 	async def order(self, ctx, *, name: str=""):
+		"""
+		Displays all members handled by the officer.
+		"""
 		member = ctx.author
 		if name:
 			member = ctx.guild.get_member_named(name)
@@ -83,28 +87,23 @@ class Activity(commands.Cog):
 
 	@commands.command()
 	@commands.guild_only()
+	@tempest.access(3)
 	async def add(self, ctx, name: str, value: int):
 		try:
-			if name.startswith('d:') or name.startswith('disc:'):
-				member = ctx.guild.get_member_named(name.split(':')[1])
-				data = db.get_member(member)
-			elif name.startswith('t:') or name.startswith('tof:'):
-				data = db.get_member_by_ign(name.split(':')[1])
-				member = ctx.guild.get_member(data[0])
-			else:
-				data = db.get_member_by_ign(name)
-				member = ctx.guild.get_member(data[0])
-			if not member:
-				raise TypeError
+			member, data = tempest.parse_name(name)
 		except TypeError:
 			return await ctx.send('Could not find member in database.')
+		except Exception as e:
+			print(e)
 		db.add_contribution(member, value)
 		await ctx.send(f"{data[1]}'s contributions have been recorded.")
 
 	@commands.command(aliases=['act'])
 	@commands.guild_only()
+	@tempest.access(3)
 	async def activity(self, ctx, name: str=''):
-		data = db.fetch_contributions(ctx.author)
+		member, data = tempest.parse_name(name)
+		data = db.fetch_contributions()
 		body = ''
 		for day in data:
 			body += f"Day: {day[3]} | {day[2]} honor.\n"
